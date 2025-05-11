4d51e87fc2ae2b8a80570cbbd6e49aa9fea29596
Akbar19 <akbarilxambaev@gmail.com>
2025-05-11 22:08:03 +0500

    Refactor URL routing to simplify registration of Subcategory, Service, and News viewsets; remove nested routing

--- api/urls.py
+++ api/urls.py
@@ -1,18 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_nested import routers
-from .views import CategoryViewSet, SubcategoryViewSet, ServiceViewSet
+from .views import CategoryViewSet, SubcategoryViewSet, ServiceViewSet, NewsViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register('categories', CategoryViewSet)
-sub_router = routers.NestedDefaultRouter(router, 'categories', lookup='category')
-sub_router.register('subcategories', SubcategoryViewSet, basename='category-subcategories')
-
-serv_router = routers.NestedDefaultRouter(sub_router, 'subcategories', lookup='subcategory')
-serv_router.register('services', ServiceViewSet, basename='subcategory-services')
-router.register('news', CategoryViewSet, basename='news')
+router.register('subcategories', SubcategoryViewSet)
+router.register('services', ServiceViewSet)
+router.register('news', NewsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
