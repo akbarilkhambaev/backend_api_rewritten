fda83609c65757c473fc138c306f6b1d20296083
Akbar19 <akbarilxambaev@gmail.com>
2025-05-11 16:08:27 +0500

    Register Subcategory, Service, and News models in admin; update serializers for Category and Subcategory

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Subcategory, Service, News
 
 admin.site.register(Category)
+admin.site.register(Subcategory)
+admin.site.register(Service)
+admin.site.register(News)
--- api/serializers.py
+++ api/serializers.py
@@ -1,15 +1,17 @@
 from rest_framework import serializers
 from .models import Category, Subcategory, Service, News
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name']
-
 class SubcategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Subcategory
-        fields = ['id', 'name', 'category']
+        fields = ['id', 'name']
+
+class CategorySerializer(serializers.ModelSerializer):
+    subcategories = SubcategorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'subcategories']
 
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
