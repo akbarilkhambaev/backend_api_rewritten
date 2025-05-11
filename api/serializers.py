d7d380e0ab429d7f99e52b35d3763af962ea8862
Akbar19 <akbarilxambaev@gmail.com>
2025-05-11 22:31:01 +0500

    Refactor serializers for Category, Subcategory, and Service; update relationships and fields

--- api/serializers.py
+++ api/serializers.py
@@ -1,20 +1,24 @@
 from rest_framework import serializers
 from .models import Category, Subcategory, Service, News
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name']
-
-class SubcategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subcategory
-        fields = ['id', 'name', 'category']
-
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
-        fields = ['id', 'name', 'description', 'subcategory']
+        fields = ['id', 'name', 'description']
+
+class SubcategorySerializer(serializers.ModelSerializer):
+    services = ServiceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Subcategory
+        fields = ['id', 'name', 'services']
+
+class CategorySerializer(serializers.ModelSerializer):
+    subcategories = SubcategorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'subcategories']
 
 class NewsSerializer(serializers.ModelSerializer):
     class Meta:
