0d42686dd41cf1298bad132a84c0bd8531aa6536
Akbar19 <akbarilxambaev@gmail.com>
2025-05-11 15:41:18 +0500

    Add serializers for Subcategory, Service, and News models

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Subcategory, Service, News
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name']
+
+class SubcategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subcategory
+        fields = ['id', 'name', 'category']
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = ['id', 'name', 'subcategory']
+
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = ['id', 'title', 'content', 'created_at', 'photo']
